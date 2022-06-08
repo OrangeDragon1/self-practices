be3e89310281f007fd713d1c824a0677aa570a65
OrangeDragon1 <wcks095@gmail.com>
2022-06-08 21:59:44 +0800

    finished sumodd

--- controlflow/core/NumberOfDaysInMonth.java
+++ controlflow/core/NumberOfDaysInMonth.java
@@ -1,11 +1,11 @@
 package controlflow.core;
 
 public class NumberOfDaysInMonth {
-    
+
     public static void main(String[] args) {
-        
+
         // System.out.println(isLeapYear(2000));
-        System.out.println(getDaysInMonth(-1, 2020));
+        System.out.println(getDaysInMonth(10, 2020));
     }
 
     public static boolean isLeapYear(int year) {
@@ -14,16 +14,16 @@ public class NumberOfDaysInMonth {
         boolean range = year >= 1 && year <= 9999;
         String divisible = "no";
 
-        if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             divisible = "yes";
-        } 
+        }
 
-        if(range) {
+        if (range) {
             switch (divisible) {
                 case "yes":
                     isLeap = true;
                     break;
-            
+
                 default:
                     break;
             }
@@ -37,30 +37,38 @@ public class NumberOfDaysInMonth {
 
         int numDays = -1;
 
-        if((month >= 1 && month <= 12) && (year >= 1 && year <= 9999)) {
-            
-        
+        if ((month >= 1 && month <= 12) && (year >= 1 && year <= 9999)) {
+
             switch (month) {
-                case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
                     numDays = 31;
                     break;
 
-                case 4: case 6: case 9: case 11:
+                case 4:
+                case 6:
+                case 9:
+                case 11:
                     numDays = 30;
                     break;
 
                 case 2:
-                    if(isLeapYear(year)) {
+                    if (isLeapYear(year)) {
                         numDays = 29;
                     } else {
                         numDays = 28;
                     }
                     break;
-                
+
                 default:
                     break;
-                }
-            
+            }
+
         }
         return numDays;
     }
--- controlflow/core/SumOdd.java
+++ controlflow/core/SumOdd.java
@@ -1,5 +1,39 @@
 package controlflow.core;
 
 public class SumOdd {
-    
+
+    public static void main(String[] args) {
+
+        // System.out.println(isOdd(69));
+        System.out.println(sumOdd(0, 10));
+    }
+
+    public static boolean isOdd(int number) {
+
+        boolean ifOdd = false;
+
+        if (number > 0) {
+            if (number % 2 != 0) {
+                ifOdd = true;
+            }
+        }
+
+        return ifOdd;
+    }
+
+    public static int sumOdd(int start, int end) {
+
+        int addOdd = 0;
+
+        if (end >= start && start > 0 && end > 0) {
+            for (int i = start; i <= end; i++) {
+                if (isOdd(i)) {
+                    addOdd += i;
+                }
+            }
+        } else {
+            addOdd = -1;
+        }
+        return addOdd;
+    }
 }
